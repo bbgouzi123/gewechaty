6b94f24696b97816d09a9286572d1308754065fd
A <49177898+bbgouzi123@users.noreply.github.com>
2026-09-17 21:08:54 +0800

    Clear repository contents

--- src/api/file.js
+++
@@ -1,6 +0,0 @@
-import {post} from '@/request/request'
-
-export const downloadFile = (data, config) => {
-  return post('/message/downloadImage', data, config)
-}
-
--- src/bot.js
+++
@@ -1,9 +0,0 @@
-import EventEmitter from './class/EMITTER.js'
-
-export const botEmitter = new EventEmitter()
-
-botEmitter.setEventNames(['message', 'all', 'room-invite', 'friendship', 'scan'])
-
-export const roomEmitter = new EventEmitter()
-
-
